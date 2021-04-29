b37676c728eebd39fcd53fbef905d527486476c6
jk_hoap <7385697@qq.com>
2021-04-29 14:19:10 +0800

    添加Eureka-client DEMO

---
+++ eureka-client/src/main/java/com/springcloud/eurekaclient/controller/ProviderController.java
@@ -0,0 +1,41 @@
+package com.springcloud.eurekaclient.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+/**
+ * @program: spring-cloud-demo2
+ * @description:
+ * @author: yaosh
+ * @create: 2021-04-29 14:09
+ */
+@Slf4j
+@RestController
+public class ProviderController {
+
+    @Autowired
+    private DiscoveryClient discoveryClient;
+
+    @RequestMapping(value = "/hello")
+    public String hello(){
+        List<String> services = discoveryClient.getServices();
+        for(String s : services){
+            log.info(s);
+        }
+        return "hello spring cloud!";
+    }
+
+    @RequestMapping(value = "/nice")
+    public String nice(){
+        List<String> services = discoveryClient.getServices();
+        for(String s : services){
+            log.info("gogogo" + s);
+        }
+        return "nice to meet you!";
+    }
+}
